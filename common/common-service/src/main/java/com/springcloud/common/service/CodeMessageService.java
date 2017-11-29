33755128abf8ba5e5453a174e2dc09cf61d3fcd3
chenkesheng <676638160@qq.com>
2017-11-29 10:27:41 +0800

    去除无用的jar引用

--- common/common-service/src/main/java/com/springcloud/common/service/CodeMessageService.java
+++ common/common-service/src/main/java/com/springcloud/common/service/CodeMessageService.java
@@ -6,8 +6,6 @@ import com.springcloud.core.entity.CodeMessage;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
-
 /**
  * 错误码对应的消息工具类
  *
