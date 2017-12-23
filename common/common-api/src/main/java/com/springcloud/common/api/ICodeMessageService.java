5d81129a5fcb7e2c9f4224c08f9d60c08ea0b0b6
cks <kesheng.chen@molbase.com>
2017-12-23 16:25:50 +0800

    修类pom

--- common/common-api/src/main/java/com/springcloud/common/api/ICodeMessageService.java
+++ common/common-api/src/main/java/com/springcloud/common/api/ICodeMessageService.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 /**
  * @author cks
  * @Date 2017/7/21.
