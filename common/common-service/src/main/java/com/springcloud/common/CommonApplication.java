b0040bac70f2c90b7670800fb4f62eba66b9fdf8
cks <kesheng.chen@molbase.com>
2018-09-18 16:54:55 +0800

    调整项目架构

--- common/common-service/src/main/java/com/springcloud/common/CommonApplication.java
+++ common/common-service/src/main/java/com/springcloud/common/CommonApplication.java
@@ -4,7 +4,10 @@ package com.springcloud.common;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * @Author: cks
@@ -12,11 +15,21 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @Package: com.springcloud.common
  * @Description:
  */
+//启用单点登陆
+//@EnableOAuth2Sso
+//向eureka注册自己作为一个服务的调用client
+ @EnableDiscoveryClient
+//注册中心 代表的是向eureka注册自己，将自己作为一个服务
+//@EnableEurekaClient
+//声明调用
+@EnableFeignClients
+//springCloud启动
 @SpringBootApplication
-//开启注册中心
-@EnableEurekaClient
-//开启熔断器
+//熔断器
 @EnableCircuitBreaker
+@EnableHystrixDashboard
+//实时刷新
+@RefreshScope
 public class CommonApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommonApplication.class, args);
---
+++ hystrix-turbine/src/main/java/com/cks/hystrix/HystrixApplication.java
@@ -0,0 +1,28 @@
+package com.cks.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * @Author: cks
+ * @Date: Created by 下午5:35 2017/11/19
+ * @Package: com.cks.hystrix
+ * @Description:
+ */
+@SpringBootApplication
+//开启注册中心
+@EnableEurekaClient
+//开启熔断器
+@EnableCircuitBreaker
+//启用Hystrix Dashboard功能
+@EnableHystrixDashboard
+@EnableTurbine
+public class HystrixApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixApplication.class, args);
+    }
+}
